a771adf6e60e4ac8a8b5fc7bbe57c81d260ad1c5
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-07-23 20:06:14 +0900

    feat : 엔티티 클래스 개발1

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,39 +1,39 @@
-package jpabook.jpashop;
-
-import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.Assert.*;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class MemberRepositoryTest {
-
-    @Autowired MemberRepository memberRepository;
-
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    public void testMember() throws Exception{
-        //given
-        Member member = new Member();
-        member.setUserName("MemberA");
-
-        //when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(savedId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
-
-        //transaction안에서는 id 값이 같으면 같은 entity로 취급(영속성)
-        Assertions.assertThat(findMember).isEqualTo(member);
-    }
-}
+//package jpabook.jpashop;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.junit.Assert.*;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class MemberRepositoryTest {
+//
+//    @Autowired MemberRepository memberRepository;
+//
+//    @Test
+//    @Transactional
+//    @Rollback(value = false)
+//    public void testMember() throws Exception{
+//        //given
+//        Member member = new Member();
+//        member.setUserName("MemberA");
+//
+//        //when
+//        Long savedId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(savedId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+//
+//        //transaction안에서는 id 값이 같으면 같은 entity로 취급(영속성)
+//        Assertions.assertThat(findMember).isEqualTo(member);
+//    }
+//}
