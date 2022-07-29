d139935f6e579aca67adbf46561d6d9792417470
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-07-29 20:08:30 +0900

    feat : 회원 등록 기능

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class MemberController {
+
+    private final MemberService memberService;
+
+    /**
+     * 회원 등록 폼
+     */
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        log.info("createFormController");
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    /**
+     * 회원 등록 기능
+     */
+    @PostMapping("members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+
+        //@Valid로 체크한 부분에 error가 발생하면 여기서 잡히게 된다.
+        if(result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+}
