730693f908286f530f8b76de305f3f00e9b61baa
dlqjagml <dlqjagml6447@naver.com>
2022-12-01 14:45:39 +0900

    feat : 회원 조회 API 수정2 - Result 클래스 추가

--- src/main/java/jpabook/jpashop/api/dto/Result.java
+++ src/main/java/jpabook/jpashop/api/dto/Result.java
@@ -1,2 +1,12 @@
-package jpabook.jpashop.api.dto;public class Result {
+package jpabook.jpashop.api.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Result<T> {
+    //response dto의 확장성을 더 크게 해준다.
+    //private int count; -> 이런식으로 더 들어가도 좋음
+    private T data;
 }
